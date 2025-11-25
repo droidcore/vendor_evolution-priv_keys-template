52231cafded1d3c77675ba6b442fe7db84d06027
droidcore <agusmahardika99@gmail.com>
2025-11-25 17:12:41 +0700

    Update make_key.sh

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=ID/ST=Yogyakarta/L=Indonesia/O=Agus/OU=Mahar/CN=Dika/emailAddress=agusmahardika99@gmail.com'
